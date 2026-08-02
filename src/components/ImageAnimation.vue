a96f850aaddaa058d534a6961ece2c935edd226c
spystar20 <hert5197@gmail.com>
2026-08-02 21:03:01 +0530

    multiple parallax

--- src/components/ImageAnimation.vue
+++ src/components/ImageAnimation.vue
@@ -16,29 +16,77 @@ onMounted(()=>{
 //     ease: 'power4.out'
 //   }
 // )
-gsap.fromTo('.parallax-container',
-    {
-backgroundPosition:'center 60%'    },{
-        backgroundPosition:'center 80%',scrollTrigger:{
-            trigger:'.parallax-container',start:'top bottom',end:'bottom top',scrub:true
-        }
+// gsap.fromTo('.parallax-container',
+//     {
+// backgroundPosition:'center 60%'    },{
+//         backgroundPosition:'center 80%',scrollTrigger:{
+//             trigger:'.parallax-container',start:'top bottom',end:'bottom top',scrub:true
+//         }
+//     }
+// )
+// const tl = gsap.timeline({
+//   scrollTrigger:{
+//     trigger:'.horizontal-scene',
+//     start:'top 20%',
+//     end:'bottom top',
+//     scrub:true,
+//     markers:true
+//   }
+// })
+// tl.to('.img-1',{xPercent:-10},0)
+// .to('.img-2',{
+//   xPercent:-25
+// },0)
+// .to('.img-3',{
+//   xPercent:20
+// },0)
+// .to('.img-4',{
+//   xPercent:35
+// },0)
+// tl.to('.layer-bg',{
+//   yPercent:5
+// },0)
+// .to('.layer-middle',{
+//   yPercent:15},0)
+
+//   .to('.layer-front',{
+//     yPercent:25
+//   },0)
+gsap.fromTo('.parallax-img',
+  {
+    yPercent: -10,
+    scale: 1.1
+  },
+  {
+    yPercent: 10,
+    scale: 1,
+
+    scrollTrigger: {
+      trigger: '.parallax-container',
+      start: 'top 80%',
+      end: 'bottom 20%',
+      scrub: true,
+      markers: true
     }
+  }
 )
 })
 </script>
 <template>
-   <section class="h-screen bg-red-500"></section>
-
-<section class="h-screen bg-black flex items-center justify-center">
+  <section class="min-h-screen bg-black"></section>
+<section class="parallax-container h-screen w-full overflow-hidden bg-black">
 
   <div
-    class="parallax-container w-full h-screen bg-cover bg-center overflow-hidden"
-    style="background-image: url('https://images.unsplash.com/photo-1497366754035-f200968a6e72?w=1200')"
-  >
-  </div>
+    class="parallax-img h-[120%] w-full bg-cover bg-center"
+    style="background-image: url('https://images.unsplash.com/photo-1497366754035-f200968a6e72?w=1600')"
+  ></div>
 
 </section>
 
-<section class="h-screen bg-blue-500"></section>
+<section class="h-screen bg-black"></section>
+
+
+
+<section class="h-screen bg-black"></section>
 </template>
 <style scoped></style>
