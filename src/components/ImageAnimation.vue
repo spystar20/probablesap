7898c1b526b40d2df3c8c00f7908044bc014de78
spystar20 <hert5197@gmail.com>
2026-08-04 20:54:39 +0530

    .

--- src/components/ImageAnimation.vue
+++ src/components/ImageAnimation.vue
@@ -52,41 +52,134 @@ onMounted(()=>{
 //   .to('.layer-front',{
 //     yPercent:25
 //   },0)
-gsap.fromTo('.parallax-img',
-  {
-    yPercent: -10,
-    scale: 1.1
-  },
-  {
-    yPercent: 10,
-    scale: 1,
+// gsap.fromTo('.parallax-img',
+//   {
+//     yPercent: -10,
+//     scale: 1.1
+//   },
+//   {
+//     yPercent: 10,
+//     scale: 1,
 
-    scrollTrigger: {
-      trigger: '.parallax-container',
-      start: 'top 80%',
-      end: 'bottom 20%',
-      scrub: true,
-      markers: true
-    }
-  }
-)
+//     scrollTrigger: {
+//       trigger: '.parallax-container',
+//       start: 'top 80%',
+//       end: 'bottom 20%',
+//       scrub: true,
+//       markers: true
+//     }
+//   }
+// )
+const images = gsap.utils.toArray('.interior-img')
+const items = gsap.utils.toArray('.project-item')
+items.forEach(item=>{
+  item.addEventListener('mouseenter',()=>{
+    gsap.to(images,{
+      opacity:1,scale:1,stagger:0.1,duration:0.5,ease:'back.out(1.7)'
+    })
+  })
+})
 })
 </script>
 <template>
   <section class="min-h-screen bg-black"></section>
-<section class="parallax-container h-screen w-full overflow-hidden bg-black">
+<!-- <section class="parallax-container h-screen w-full overflow-hidden bg-black">
 
   <div
     class="parallax-img h-[120%] w-full bg-cover bg-center"
     style="background-image: url('https://images.unsplash.com/photo-1497366754035-f200968a6e72?w=1600')"
   ></div>
 
+</section> -->
+
+
+<section class="gallery-section min-h-screen bg-black text-white flex items-center justify-center">
+
+  <div class="project-list">
+
+    <div class="project-item" data-project="interior">
+      Interior Design
+    </div>
+
+    <div class="project-item" data-project="architecture">
+      Architecture
+    </div>
+
+    <div class="project-item" data-project="branding">
+      Branding
+    </div>
+
+    <div class="project-item" data-project="fashion">
+      Fashion
+    </div>
+
+  </div>
+
+  <!-- Images -->
+  <div class="image-container">
+
+    <img
+      class="project-image interior-img img-1"
+      src="https://images.unsplash.com/photo-1497366754035-f200968a6e72?w=800"
+    />
+
+    <img
+      class="project-image interior-img img-2"
+      src="https://images.unsplash.com/photo-1497366811353-6870744d04b2?w=800"
+    />
+
+    <img
+      class="project-image interior-img img-3"
+      src="https://images.unsplash.com/photo-1497366216548-37526070297c?w=800"
+    />
+
+  </div>
+
 </section>
 
 <section class="h-screen bg-black"></section>
-
-
-
-<section class="h-screen bg-black"></section>
 </template>
-<style scoped></style>
+<style scoped>
+.gallery-section {
+  position: relative;
+}
+
+.project-list {
+  position: relative;
+  z-index: 10;
+}
+
+.project-item {
+  font-size: 4rem;
+  cursor: pointer;
+}
+
+.image-container {
+  position: absolute;
+  inset: 0;
+  pointer-events: none;
+}
+
+.project-image {
+  position: absolute;
+  width: 250px;
+  height: 320px;
+  object-fit: cover;
+  opacity: 0;
+  scale: 0.5;
+}
+.img-1 {
+  top: 20%;
+  left: 25%;
+}
+
+.img-2 {
+  top: 40%;
+  left: 45%;
+}
+
+.img-3 {
+  top: 15%;
+  left: 60%;
+}
+</style>
